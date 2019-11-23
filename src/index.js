475a2bab407a3c13333d21df4fb9893aefffab67
Pranjal Daga <pranjaldaga@gmail.com>
2019-11-22 20:47:12 -0800

    Javascript Expression in JSX Practice

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from "react-dom";
 const fname = "Pranjal";
 const lname = "Daga";
 const num = 42;
+const year = new Date().getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1>Hello {`${fname} ${lname}`}!</h1>
-    <p>Life is {num}</p>
+    <p>Created by {`${fname} ${lname}`}</p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
