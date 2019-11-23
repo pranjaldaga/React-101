cc351f7187968ddd43114c040e851ff3a950099f
Pranjal Daga <pranjaldaga@gmail.com>
2019-11-22 20:39:45 -0800

    Javascript Expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "Pranjal";
+const lname = "Daga";
+const num = 42;
+
 ReactDOM.render(
   <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-    </ul>
+    <h1>Hello {`${fname} ${lname}`}!</h1>
+    <p>Life is {num}</p>
   </div>,
   document.getElementById("root")
 );
