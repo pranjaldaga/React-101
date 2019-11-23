a33d7294e97d6409e8416e2bc191f73fba767b93
Pranjal Daga <pranjaldaga@gmail.com>
2019-11-22 20:25:23 -0800

    jsx-code-challenge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Yo World!</h1>
-    <p>Pranjal's first React page</p>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
