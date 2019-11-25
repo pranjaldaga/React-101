bccc0d674347de3293da020a96cb9a98e7373bc6
Pranjal Daga <pranjaldaga@gmail.com>
2019-11-25 00:43:19 -0800

    ES6 Import/Export Practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import * as operator from "./calculator";
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>{operator.add(1, 2)}</li>
+    <li>{operator.multiply(2, 3)}</li>
+    <li>{operator.subtract(7, 2)}</li>
+    <li>{operator.divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
