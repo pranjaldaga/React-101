3d9b18ace22de5fc9152bcaf807ff70d688c92e6
Pranjal Daga <pranjaldaga@gmail.com>
2019-11-24 23:35:41 -0800

    React Components Heading

--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
-//Create a React app from scratch.
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
-
 import React from "react";
 import ReactDOM from "react-dom";
-
-const date = new Date();
-const time = date.getHours();
-const customStyle = {
-  color: "red"
-};
-//console.log(time)
-let greeting;
-if (time < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (time < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Evening";
-  customStyle.color = "blue";
-}
+import Heading from "./Heading";
 
 ReactDOM.render(
   <div>
-    <h1 className="heading" style={customStyle}>
-      {" "}
-      {greeting}{" "}
-    </h1>
+    <Heading />
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
